23441e0ba915b0935cb920e4772b2b070d6eb61b
M-Alem <32704888+M-Alem@users.noreply.github.com>
2019-03-25 14:45:59 +0100

    refactored tests

--- Spring-BlockChain/Spring-BlockChain/src/test/java/edu/ap/spring/test/SpringTest1.java
+++ Spring-BlockChain/Spring-BlockChain/src/test/java/edu/ap/spring/test/SpringTest1.java
@@ -7,11 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
 
+import java.io.PrintStream;
+
 import org.junit.*;
 import org.junit.runners.MethodSorters;
 
 import edu.ap.spring.service.*;
 import edu.ap.spring.transaction.Transaction;
+import edu.ap.spring.transaction.TransactionOutput;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,7 +25,12 @@ public class SpringTest1 {
 	private BlockChain bChain;
 	@Autowired
 	private Wallet coinbase, walletA, walletB;
-	private Transaction genesisTransaction;
+	@Autowired
+	private Block genesis, block1;
+	@Autowired
+	private Transaction genesisTransaction;;
+
+	PrintStream console;
 
 	@Before
 	public void init() {
@@ -31,35 +39,47 @@ public class SpringTest1 {
 		walletA.generateKeyPair();
 		walletB.generateKeyPair();
 
-		//create genesis transaction, which sends 100 coins to walletA:
-		genesisTransaction = new Transaction(coinbase.getPublicKey(), walletA.getPublicKey(), 100f);
-		genesisTransaction.generateSignature(coinbase.getPrivateKey());	 // manually sign the genesis transaction	
+		// create genesis transaction, which sends 100 coins to walletA:
+		genesisTransaction.setSender(coinbase.publicKey);
+		genesisTransaction.setRecipient(walletA.publicKey);
+		genesisTransaction.setValue(100f);
+		genesisTransaction.setInputs(null);
+		genesisTransaction.generateSignature(coinbase.privateKey); // manually sign the genesis transaction
 		genesisTransaction.transactionId = "0"; // manually set the transaction id
-						
-		//creating and Mining Genesis block
-		Block genesis = new Block();
+		genesisTransaction.outputs.add(new TransactionOutput(genesisTransaction.recipient, genesisTransaction.value,
+				genesisTransaction.transactionId)); // manually add the Transactions Output
+		bChain.getUTXOs().put(genesisTransaction.outputs.get(0).id, genesisTransaction.outputs.get(0)); // it's
+																										// important to
+																										// store our
+																										// first
+																										// transaction
+																										// in the UTXOs
+																										// list.
+		// creating and Mining Genesis block
 		genesis.setPreviousHash("0");
+		genesis.setTimeStamp();
+		genesis.calculateHash();
 		genesis.addTransaction(genesisTransaction, bChain);
 		bChain.addBlock(genesis);
 	}
-	
+
 	@After
 	public void after() {
-		// cleanup
- 	}
+		System.setOut(console);
+	}
 
 	@Test
 	public void transaction1() {
 		Block block = new Block();
 		block.setPreviousHash(bChain.getLastHash());
-			
+
 		try {
 			block.addTransaction(walletA.sendFunds(walletB.getPublicKey(), 40f), bChain);
-		} 
-		catch(Exception e) {}
-		
+		} catch (Exception e) {
+		}
+
 		bChain.addBlock(block);
-		
+
 		assertEquals(60f, walletA.getBalance(), 0);
 		assertEquals(40f, walletB.getBalance(), 0);
 	}
--- Spring-BlockChain/Spring-BlockChain/src/test/java/edu/ap/spring/test/SpringTest2.java
+++ Spring-BlockChain/Spring-BlockChain/src/test/java/edu/ap/spring/test/SpringTest2.java
@@ -23,52 +23,58 @@ import org.junit.runners.MethodSorters;
 public class SpringTest2 {
 
 	@Test
-  public void transaction1() throws Exception {
- 		TestRestTemplate testRestTemplate = new TestRestTemplate();
-		ResponseEntity<String> response1 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletA", String.class);
-		ResponseEntity<String> response2 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletB", String.class);
-  
+	public void transaction1() throws Exception {
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
+		ResponseEntity<String> response1 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletA",
+				String.class);
+		ResponseEntity<String> response2 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletB",
+				String.class);
+
 		assertTrue(response1.getBody().contains("100.0"));
 		assertTrue(response2.getBody().contains("0.0"));
-  }
+	}
 
 	@Test
-  public void transaction2() throws Exception {
- 		TestRestTemplate testRestTemplate = new TestRestTemplate();
+	public void transaction2() throws Exception {
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 		map.add("wallet1", "walletA");
 		map.add("wallet2", "walletB");
 		map.add("amount", "30.0");
 
 		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-		testRestTemplate.postForEntity("http://localhost:8080/transaction", request , String.class);
-  
-		ResponseEntity<String> response1 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletA", String.class);
-		ResponseEntity<String> response2 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletB", String.class);
+		testRestTemplate.postForEntity("http://localhost:8080/transaction", request, String.class);
 
-  	assertTrue(response1.getBody().contains("70.0"));
+		ResponseEntity<String> response1 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletA",
+				String.class);
+		ResponseEntity<String> response2 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletB",
+				String.class);
+
+		assertTrue(response1.getBody().contains("70.0"));
 		assertTrue(response2.getBody().contains("30.0"));
-  }
+	}
 
 	@Test
-  public void transaction3() throws Exception {
- 		TestRestTemplate testRestTemplate = new TestRestTemplate();
+	public void transaction3() throws Exception {
+		TestRestTemplate testRestTemplate = new TestRestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 		map.add("wallet1", "walletB");
 		map.add("wallet2", "walletA");
 		map.add("amount", "20.0");
 
 		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-		testRestTemplate.postForEntity("http://localhost:8080/transaction", request , String.class);
-  
-		ResponseEntity<String> response1 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletA", String.class);
-		ResponseEntity<String> response2 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletB", String.class);
+		testRestTemplate.postForEntity("http://localhost:8080/transaction", request, String.class);
 
-  	assertTrue(response1.getBody().contains("90.0"));
+		ResponseEntity<String> response1 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletA",
+				String.class);
+		ResponseEntity<String> response2 = testRestTemplate.getForEntity("http://localhost:8080/balance/walletB",
+				String.class);
+
+		assertTrue(response1.getBody().contains("90.0"));
 		assertTrue(response2.getBody().contains("10.0"));
-  }
+	}
 }
